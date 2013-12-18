225f0f4a87e9378451d236b3aacbacb23d0e0f40
R.D. Ross <rross@spkaa.com>
2013-12-17 16:29:49 -0800

    Add oata class to HelloWorld.java file

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,7 +2,10 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class HelloWorldApp {
+
+package oata;
+
+public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
